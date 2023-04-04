9beb02fb789addfab98010a9b0ba2e3ca793a609
Marcos Rost <marcos.rost@ifro.edu.br>
2023-04-04 10:42:02 -0400

    Aterada exibicao do nome + matricula

--- cs_modules/permissoes.administradas.ShowNames.js
+++ cs_modules/permissoes.administradas.ShowNames.js
@@ -2,6 +2,6 @@ var info = "";
 var names = document.getElementsByClassName("ancoraSigla");
 for (var i = 0; i < names.length; i += 4) {
     info = names[i].text;  
-    names[i].text = names[i].title;
+    names[i].text = [names[i].title, ' (', info, ')'].join('');
     names[i].title = info
 }
